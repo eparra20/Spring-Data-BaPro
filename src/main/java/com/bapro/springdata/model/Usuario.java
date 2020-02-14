1f21dce44cca8fa069723a4edf8cb1769041fdf4
Edu <eduardo28709@gmail.com>
2020-02-13 23:50:35 -0300

    configuracion y creacion de Modelo y Repositorio Usuario

---
+++ src/main/java/com/bapro/springdata/model/Usuario.java
@@ -0,0 +1,88 @@
+package com.bapro.springdata.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/**
+ * Tabla o Entidad Usuario
+ * @author Eduar
+ *
+ */
+@Entity
+public class Usuario {
+
+	/**
+	 * Esta sera nuestra Primary KEY que adicionalmente estamos indicando que sera AUTO_INCREMENT
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, unique = true)
+	private String dni;
+	
+	@Column(nullable = false)
+	private String nombre;
+	
+	private String apellido;
+	
+	@Column(nullable = false)
+	private Boolean activo;
+	
+	public Usuario() {
+		
+	}
+	
+
+	public Usuario(Integer id, String dni, String nombre, String apellido, Boolean activo) {
+		this.id = id;
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.activo = activo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Boolean getActivo() {
+		return activo;
+	}
+
+	public void setActivo(Boolean activo) {
+		this.activo = activo;
+	}
+	
+	
+}
---
+++ src/main/java/com/bapro/springdata/repository/UsuarioJpaRepository.java
@@ -0,0 +1,11 @@
+package com.bapro.springdata.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bapro.springdata.model.Usuario;
+
+
+public interface UsuarioJpaRepository extends JpaRepository<Usuario, Integer>{
+
+}
